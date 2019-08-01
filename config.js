86cc4a4298c93b43581ee3c6c5fdca0f2d379ec0
imhaider <53440791+imhaider@users.noreply.github.com>
2019-08-01 12:05:10 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NjA2NDExMzc0ODgwNDg5NDkz.XUKqxg.I3n3WOz7gcpsOKGpsDrz88Sv7BQ', 
+    YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4', 
+    prefix: '4',
+    devs: ['524901565472047116']
+}
